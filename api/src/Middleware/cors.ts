5f65ab7fe5423886ea860148624a4c13d93fb3c2
glutix <ferreyra.gluti@gmail.com>
2024-03-10 15:29:37 -0300

    fix error cors

--- api/src/Middleware/cors.ts
+++ api/src/Middleware/cors.ts
@@ -2,14 +2,13 @@ import { Response, Request, NextFunction } from "express";
 
 const cors = (req: Request, res: Response, next: NextFunction) => {
   const domain = "http://localhost:3000";
-
   res.header("Access-Control-Allow-Origin", domain);
-  res.header("Aceess-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 };
 
